d0947314fa5a205aa896e661c75bda0b8c2156d9
Pierre <pierre.sjoberg@kau.se>
2021-04-20 11:41:36 +0200

    F slutkod

--- F2/Kodexempel_1/script.js
+++ F2/Kodexempel_1/script.js
@@ -1,4 +1,90 @@
-window.addEventListener('load', loadData);
+window.addEventListener('load', init);
+
+function init() {
+
+  //Göm preloader
+  document.querySelector('#preloader').classList.add('d-none');
+
+  document.querySelector('#content').classList.add('d-flex','flex-wrap')
+
+  document.querySelector('#search-form').addEventListener('submit', handleSearchFormSubmit);
+
+
+}
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+  
+  let searchField = document.querySelector('#search');
+  let content = document.querySelector('#content');
+
+  content.innerHTML=null;
+  document.querySelector('#preloader').classList.remove('d-none');
+  search(searchField.value, content);
+}
+
+function search(query, container) {
+
+  window.fetch('https://restcountries.eu/rest/v2/name/' + encodeURIComponent(query))
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log(data);
+      document.querySelector('#preloader').classList.add('d-none');
+
+      for(let countryData of data) {
+        
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.style.minWidth = '20rem';
+        card.style.width = '25%';
+        container.appendChild(card);
+
+        let cardImage = document.createElement('img');
+        cardImage.className = 'card-img-top';
+        cardImage.src = countryData.flag;
+        card.appendChild(cardImage)
+
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        card.appendChild(cardBody);
+
+        let cardTitle = document.createElement('h5');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = countryData.name;
+        cardBody.appendChild(cardTitle)
+
+        let capital = document.createElement('p');
+        capital.className = 'card-text';
+        capital.innerHTML = '<b>Capital:</b> ' + countryData.capital;
+        cardBody.appendChild(capital)
+
+        let area = document.createElement('p');
+        area.className = 'card-text';
+        if (countryData.area == null) {
+          area.innerHTML = '<b>Area:</b> n/a';
+        } else {
+          area.innerHTML = '<b>Area:</b> ' + countryData.area.toLocaleString('en') + ' km<sup>2</sup>';
+        }
+        cardBody.appendChild(area)
+
+        let population = document.createElement('p');
+        population.className = 'card-text';
+        population.innerHTML = '<b>Population:</b> ' + countryData.population.toLocaleString('en');
+        cardBody.appendChild(population)
+
+      }
+
+    });
+
+
+
+
+}
+
+
+
+
 
 function loadData() {
   let container = document.querySelector('#content');
--- F2/Kodexempel_2/script.js
+++ F2/Kodexempel_2/script.js
@@ -1,11 +1,11 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', () => {
 
   let form = document.getElementById('search-form');
   let content = document.getElementById('content');
   let searchField = document.getElementById('search');
   let tbody = document.getElementById('tbody');
 
-  form.addEventListener('submit', function(event){
+  form.addEventListener('submit', (event) =>{
     event.preventDefault();
     search(searchField.value, tbody);
   });
@@ -17,7 +17,7 @@ window.addEventListener('load', function() {
  * @param {string} query The user’s search query
  * @param {HTMLElement} container The <tbody> element that the result will be printed to
  */
-function search(query, container){
+let search = (query, container) => {
   container.innerHTML = '';
   window.fetch('https://restcountries.eu/rest/v2/name/' + encodeURIComponent(query))
     .then(response => response.json())
@@ -25,6 +25,31 @@ function search(query, container){
       console.log(data);
       data.forEach(item => {
         //Gör något;
+        let row = document.createElement('tr');
+        container.appendChild(row);
+
+        let name = document.createElement('td');
+        name.textContent = item.name;
+        row.appendChild(name);
+
+        let capital = document.createElement('td');
+        capital.textContent = item.capital;
+        row.appendChild(capital);
+
+        let area = document.createElement('td');
+        area.classList.add('text-right');
+        if(item.area != null) {
+          area.innerHTML = item.area.toLocaleString('en') + ' km<sup>2</sup>';
+        }
+        row.appendChild(area);
+
+        let population = document.createElement('td');
+        population.classList.add('text-right');
+        if (item.population != null) {
+          population.innerHTML = item.population.toLocaleString('en');
+        }
+        row.appendChild(population);
+
       })
     });
 }
